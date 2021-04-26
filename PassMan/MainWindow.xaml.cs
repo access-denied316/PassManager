2bf36a0ff6381737107e226357919ce1941aa847
access-denied316 <jacobs.owen@gmail.com>
2021-04-26 18:01:47 -0400

    yeah, go

--- PassMan/MainWindow.xaml.cs
+++ PassMan/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
+
 namespace PassMan
 {
     /// <summary>
@@ -31,12 +33,23 @@ namespace PassMan
             DockPanel dock = new DockPanel();
             //add site text box
             TextBox siteBx = new TextBox();
-            siteBx.MinWidth = 50;
+            siteBx.MinWidth = 60;
+            siteBx.Text = "Site";
+            siteBx.GotFocus += new RoutedEventHandler(textBox_GotFocus);
             dock.Children.Add(siteBx);
-            
+
+            //add user text box
+            TextBox userBx = new TextBox();
+            userBx.MinWidth = 60;
+            userBx.Text = "Username";
+            userBx.GotFocus += new RoutedEventHandler(textBox_GotFocus);
+            dock.Children.Add(userBx);
+
             //add pass text box
             TextBox passBx = new TextBox();
-            passBx.MinWidth = 50;
+            passBx.MinWidth = 60;
+            passBx.Text = "Password";
+            passBx.GotFocus += new RoutedEventHandler(textBox_GotFocus);
             dock.Children.Add(passBx);
 
             //add remove button
@@ -49,9 +62,61 @@ namespace PassMan
             passLsBx.Items.Insert(0, dock);
         }
 
+        private void textBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            textBox.Dispatcher.BeginInvoke(new Action(() => textBox.SelectAll()));
+        }
+
         private void genPass_Click(object sender, RoutedEventArgs e)
         {
+            string cap = "";
+            for (int i = 'A'; i < 'Z' + 1; i++)
+            {
+                cap += (char)i;
+            }
+            string low = "";
+            for (int i = 97; i < 123; i++)
+            {
+                low += (char)i;
+            }
 
+            string num = "0123456789";
+            string spe = "!@#$%^&*";
+
+
+            //get available characters
+            string available = "";
+
+            if (capCB.IsChecked == true)
+            {
+                available += cap;
+            }
+
+            if (lowCB.IsChecked == true)
+            {
+                available += low;
+            }
+
+            if (numCB.IsChecked == true)
+            {
+                available += num;
+            }
+
+            if (speCB.IsChecked == true)
+            {
+                available += spe;
+            }
+
+            passPreview.Content = available;
+
+        }
+
+        private string getAvailChars(string cap, string low, string num, string spe)
+        {
+            
+
+            return available;
         }
 
         private void remBtn_Click(object sender, RoutedEventArgs e)
@@ -83,6 +148,18 @@ namespace PassMan
         private void loadBtn_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Data cannot be loaded at this time.\nSorry for any inconvenience.");
+        }
+
+        private void clearAll_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                passLsBx.Items.Clear();
+            }
+            catch
+            {
+                MessageBox.Show("There is nothing to remove!");
+            }
         }
     }
 }
